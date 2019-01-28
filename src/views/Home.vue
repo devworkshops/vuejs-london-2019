b5bb0825e565891ba2f2db8c68db8f87db3ac4db
Thiago Passos <thiago@passos.com.au>
2019-01-29 00:25:36 +1000

    linting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-
+    name: 'home'
 }
 </script>
