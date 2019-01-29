d2ebd921f4971e42ee0aad4527677fa7493a0578
Thiago Passos <thiago@passos.com.au>
2019-01-30 01:06:37 +1000

    Updating home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-4">NDC London 2019 - Mastering Vue.js</h1>
+      <p
+        class="lead"
+      >Thank you for joining us in this 2-day workshop. Hope you have enjoyed.</p>
+      <hr class="my-4">
+      <p>If you want to go over the content again, the <a target="_blank" href="https://devworkshops.gitbook.io/masteringvuejs">instructions</a> and <a target="_blank" href="https://github.com/devworkshops/vuejs-london-2019">code</a> are available for you.</p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -10,3 +16,10 @@ export default {
     name: 'home'
 }
 </script>
+
+<style scoped>
+h1 img {
+    max-height: 100%;
+}
+</style>
+
